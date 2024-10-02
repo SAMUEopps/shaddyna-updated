383436ba93fec5a31e0301b7fde533522f298896
SAMUEopps <samuelhope805@gmail.com>
2024-10-02 06:58:57 -0700

    third commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const local_api = "https://shaddyna-updated-server.onrender.com";
+const local_api = "https://shaddyna-updated-server.onrender.com/api";
 //const local_api = "http://localhost:5000/api";
 
 

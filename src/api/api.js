cec3fdc077a9ed5d7c40f29902a3e52335fc0a3f
SAMUEopps <samuelhope805@gmail.com>
2024-10-02 06:36:12 -0700

    third commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const local_api = "https://ecom-backend-iuv5.onrender.com/api";
-const local_api = "http://localhost:5000/api";
+const local_api = "https://shaddyna-updated-server.onrender.com";
+//const local_api = "http://localhost:5000/api";
 
 
 const api = axios.create({
